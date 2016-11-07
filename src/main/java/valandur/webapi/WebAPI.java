a6800be245ec6155d01d077f0972ba06ddf0f5e6
Marco Crespi <mrc@flxlabs.com>
2016-11-07 12:40:13 -0600

    update(config): Add config file [WIP] & jetty logging

--- src/main/java/valandur/webapi/WebAPI.java
+++ src/main/java/valandur/webapi/WebAPI.java
@@ -1,10 +1,15 @@
 package valandur.webapi;
 
 import com.google.inject.Inject;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.apache.commons.lang3.tuple.Triple;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.log.Log;
 import org.slf4j.Logger;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
@@ -14,7 +19,9 @@ import org.spongepowered.api.event.message.MessageChannelEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
 import valandur.webapi.handlers.Handlers;
+import valandur.webapi.misc.JettyLogger;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -48,24 +55,41 @@ public class WebAPI {
         return WebAPI.instance.logger;
     }
 
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+    private ConfigurationNode rootNode;
+
+    private String serverHost;
+    private int serverPort;
     private Server server;
 
     @Listener
     public void onPreInitialization(GamePreInitializationEvent event) {
         WebAPI.instance = this;
+
+        try {
+            rootNode = configManager.load();
+            serverHost = rootNode.getNode("server", "host").getString("localhost");
+            serverPort = rootNode.getNode("server", "port").getInt(8080);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
         logger.info("Starting Web Server...");
 
+        Log.setLog(new JettyLogger());
+
         try {
             server = new Server();
 
             // HTTP connector
             ServerConnector http = new ServerConnector(server);
-            http.setHost("localhost");
-            http.setPort(8080);
+            http.setHost(serverHost);
+            http.setPort(serverPort);
             http.setIdleTimeout(30000);
 
             server.addConnector(http);

a82ea95b129ee694016899e7efc01f1fd11f1971
Valandur <marco@aof.gg>
2016-11-15 11:20:00 -0600

    chore(version): Bump version number

--- src/main/java/valandur/webapi/WebAPI.java
+++ src/main/java/valandur/webapi/WebAPI.java
@@ -57,7 +57,7 @@ public class WebAPI {
     public static final String NAME = "Web-API";
     public static final String URL = "https://github.com/Valandur/Web-API";
     public static final String DESCRIPTION = "Access Minecraft through a Web API";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     private static WebAPI instance;
     public static WebAPI getInstance() {

f63934a5a8e4e74ff9c4eebd58779884b1975b7f
Valandur <marco@aof.gg>
2017-03-14 17:00:09 +0100

    feat(json): Add serializers for known objects

---
+++ src/main/java/valandur/webapi/json/EntitySerializer.java
@@ -0,0 +1,27 @@
+package valandur.webapi.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.spongepowered.api.entity.Entity;
+import valandur.webapi.cache.CachedEntity;
+
+import java.io.IOException;
+
+public class EntitySerializer extends StdSerializer<Entity> {
+
+    public EntitySerializer() {
+        this(null);
+    }
+
+    public EntitySerializer(Class<Entity> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Entity value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        boolean details = provider.isEnabled(MapperFeature.AUTO_DETECT_CREATORS);
+        gen.writeRawValue(JsonConverter.toString(CachedEntity.copyFrom(value, details), details));
+    }
+}
---
+++ src/main/java/valandur/webapi/json/WorldSerializer.java
@@ -0,0 +1,27 @@
+package valandur.webapi.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.spongepowered.api.world.World;
+import valandur.webapi.cache.CachedWorld;
+
+import java.io.IOException;
+
+public class WorldSerializer extends StdSerializer<World> {
+
+    public WorldSerializer() {
+        this(null);
+    }
+
+    public WorldSerializer(Class<World> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(World value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        boolean details = provider.isEnabled(MapperFeature.AUTO_DETECT_CREATORS);
+        gen.writeRawValue(JsonConverter.toString(CachedWorld.copyFrom(value, details), details));
+    }
+}

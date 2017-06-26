f0085062c0fd88bb2a40d6e09396c989848d4931
Valandur <marco@aof.gg>
2017-06-26 16:48:55 +0200

    feat(api): Add Web-API api

---
+++ webapi-api/src/main/java/valandur/webapi/api/annotation/WebAPIRoute.java
@@ -0,0 +1,14 @@
+package valandur.webapi.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface WebAPIRoute {
+    String method();
+    String path();
+    String perm() default "";
+}
--- webapi-api/src/main/java/valandur/webapi/api/annotation/WebAPIServlet.java
+++ webapi-api/src/main/java/valandur/webapi/api/annotation/WebAPIServlet.java
@@ -1,4 +1,4 @@
-package valandur.webapi.annotation;
+package valandur.webapi.api.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,9 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface WebAPISpec {
-    String method();
-    String path();
-    String perm() default "";
+@Target(ElementType.TYPE)
+public @interface WebAPIServlet {
+    String basePath();
 }

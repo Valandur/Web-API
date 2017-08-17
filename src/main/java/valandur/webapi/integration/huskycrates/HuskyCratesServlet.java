0ac5e456d1597e8bc10842c0b98f80c2f832a9bf
Valandur <marco@aof.gg>
2017-08-17 15:33:55 +0200

    fix(integration): Run husky crates plugin methods on the main server thread

--- src/main/java/valandur/webapi/integration/huskycrates/HuskyCratesServlet.java
+++ src/main/java/valandur/webapi/integration/huskycrates/HuskyCratesServlet.java
@@ -3,6 +3,7 @@ package valandur.webapi.integration.huskycrates;
 import com.codehusky.huskycrates.HuskyCrates;
 import com.codehusky.huskycrates.crate.VirtualCrate;
 import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.util.ConcurrentHashSet;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.plugin.PluginContainer;
 import valandur.webapi.WebAPI;
@@ -13,9 +14,8 @@ import valandur.webapi.json.JsonService;
 import valandur.webapi.servlet.ServletData;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @WebAPIServlet(basePath = "husky")
 public class HuskyCratesServlet extends WebAPIBaseServlet {
@@ -26,27 +26,55 @@ public class HuskyCratesServlet extends WebAPIBaseServlet {
         json.registerSerializer(CachedCrateReward.class, CachedCrateRewardSerializer.class);
     }
 
-    @WebAPIEndpoint(method = HttpMethod.GET, path = "crate", perm = "husky")
-    public void getCrates(ServletData data) {
+
+    private HuskyCrates getHuskyPlugin(ServletData data) {
         Optional<PluginContainer> optContainer = Sponge.getPluginManager().getPlugin("huskycrates");
         if (!optContainer.isPresent()) {
             data.sendError(HttpServletResponse.SC_NOT_FOUND, "HuskyCrates plugin not found");
-            return;
+            return null;
         }
 
         Optional<?> optPlugin = optContainer.get().getInstance();
         if (!optPlugin.isPresent()) {
             data.sendError(HttpServletResponse.SC_NOT_FOUND, "HuskyCrates plugin instance not found");
-            return;
+            return null;
         }
 
-        List<CachedCrate> crates = new ArrayList<>();
-        HuskyCrates plugin = (HuskyCrates)optPlugin.get();
-        for (VirtualCrate crate : plugin.getCrateUtilities().crateTypes.values()) {
-            crates.add(new CachedCrate(crate));
-        }
+        return (HuskyCrates)optPlugin.get();
+    }
+
+    @WebAPIEndpoint(method = HttpMethod.GET, path = "crate", perm = "list")
+    public void getCrates(ServletData data) {
+        HuskyCrates plugin = getHuskyPlugin(data);
+        if (plugin == null) return;
+
+        Set<CachedCrate> crates = new ConcurrentHashSet<>();
+        WebAPI.runOnMain(() -> {
+            for (VirtualCrate crate : plugin.getCrateUtilities().crateTypes.values()) {
+                crates.add(new CachedCrate(crate));
+            }
+        });
 
         data.addJson("ok", true, false);
         data.addJson("crates", crates, data.getQueryParam("details").isPresent());
+    }
+
+    @WebAPIEndpoint(method = HttpMethod.GET, path = "crate/:id", perm = "one")
+    public void getCrate(ServletData data, String id) {
+        HuskyCrates plugin = getHuskyPlugin(data);
+        if (plugin == null) return;
+
+        Optional<CachedCrate> optRes = WebAPI.runOnMain(() -> {
+            VirtualCrate crate = plugin.crateUtilities.getVirtualCrate(id);
+            if (crate == null) {
+                data.sendError(HttpServletResponse.SC_NOT_FOUND, "Crate not found");
+                return null;
+            }
+
+            return new CachedCrate(crate);
+        });
+
+        data.addJson("ok", optRes.isPresent(), false);
+        data.addJson("crate", optRes.orElse(null), true);
     }
 }

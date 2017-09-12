589425b7428c33e05f2dbebc66e8d779ce41728a
Valandur <marco@aof.gg>
2017-09-12 11:36:50 +0200

    feat(servlet-player): Start adding player update functionality

--- src/main/java/valandur/webapi/servlet/player/PlayerServlet.java
+++ src/main/java/valandur/webapi/servlet/player/PlayerServlet.java
@@ -2,13 +2,18 @@ package valandur.webapi.servlet.player;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.eclipse.jetty.http.HttpMethod;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
 import org.spongepowered.api.util.Tuple;
+import valandur.webapi.WebAPI;
 import valandur.webapi.api.annotation.WebAPIEndpoint;
 import valandur.webapi.api.annotation.WebAPIServlet;
+import valandur.webapi.api.cache.entity.ICachedEntity;
 import valandur.webapi.api.cache.player.ICachedPlayer;
 import valandur.webapi.api.servlet.WebAPIBaseServlet;
 import valandur.webapi.cache.player.CachedPlayer;
 import valandur.webapi.servlet.ServletData;
+import valandur.webapi.servlet.entity.UpdateEntityRequest;
 import valandur.webapi.util.Util;
 
 import javax.servlet.http.HttpServletResponse;
@@ -42,8 +47,47 @@ public class PlayerServlet extends WebAPIBaseServlet {
 
     @WebAPIEndpoint(method = HttpMethod.PUT, path = "/:player", perm = "change")
     public void updatePlayer(ServletData data, CachedPlayer player) {
-        data.addJson("ok", true, false);
-        data.addJson("player", player, true);
+        Optional<UpdatePlayerRequest> optReq = data.getRequestBody(UpdatePlayerRequest.class);
+        if (!optReq.isPresent()) {
+            data.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid player data: " + data.getLastParseError().getMessage());
+            return;
+        }
+
+        final UpdatePlayerRequest req = optReq.get();
+
+        Optional<ICachedEntity> resPlayer = WebAPI.runOnMain(() -> {
+            Optional<?> optLive = player.getLive();
+            if (!optLive.isPresent())
+                return null;
+
+            Player live = (Player)optLive.get();
+
+            if (req.getVelocity() != null) {
+                live.setVelocity(req.getVelocity());
+            }
+
+            if (req.getRotation() != null) {
+                live.setRotation(req.getRotation());
+            }
+
+            if (req.getScale() != null) {
+                live.setRotation(req.getScale());
+            }
+
+            if (req.getDamage() != null) {
+                UpdateEntityRequest.DamageRequest dmgReq = req.getDamage();
+                DamageSource.Builder builder = DamageSource.builder();
+                if (dmgReq.getDamageType().isPresent())
+                    builder.type(dmgReq.getDamageType().get());
+
+                live.damage(req.getDamage().getAmount(), builder.build());
+            }
+
+            return cacheService.updateEntity(live);
+        });
+
+        data.addJson("ok", resPlayer.isPresent(), false);
+        data.addJson("player", resPlayer.orElse(null), true);
     }
 
     @WebAPIEndpoint(method = HttpMethod.POST, path = "/:player/method", perm = "method")
---
+++ src/main/java/valandur/webapi/servlet/player/UpdatePlayerRequest.java
@@ -0,0 +1,8 @@
+package valandur.webapi.servlet.player;
+
+import valandur.webapi.servlet.entity.UpdateEntityRequest;
+
+public class UpdatePlayerRequest extends UpdateEntityRequest {
+
+
+}

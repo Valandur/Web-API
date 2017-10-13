77130564f5b1bcea79f0be2bb3196e65e953ce39
Valandur <marco@aof.gg>
2017-10-13 16:54:41 +0200

    feat(message): Add getting all sent messages and details about a single sent message

--- src/main/java/valandur/webapi/servlet/MessageServlet.java
+++ src/main/java/valandur/webapi/servlet/MessageServlet.java
@@ -1,17 +1,41 @@
 package valandur.webapi.servlet;
 
 import org.eclipse.jetty.http.HttpMethod;
-import valandur.webapi.api.servlet.Endpoint;
-import valandur.webapi.api.servlet.Servlet;
+import valandur.webapi.api.message.IMessage;
 import valandur.webapi.api.servlet.BaseServlet;
+import valandur.webapi.api.servlet.Endpoint;
+import valandur.webapi.api.servlet.IServletData;
+import valandur.webapi.api.servlet.Servlet;
 import valandur.webapi.message.Message;
 import valandur.webapi.servlet.base.ServletData;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Servlet(basePath = "message")
 public class MessageServlet extends BaseServlet {
+
+    @Endpoint(method = HttpMethod.GET, path = "/", perm = "list")
+    public void getMessages(IServletData data) {
+        List<IMessage> msgs = messageService.getMessages();
+
+        data.addData("ok", true, false);
+        data.addData("messages", msgs, data.getQueryParam("details").isPresent());
+    }
+
+    @Endpoint(method = HttpMethod.GET, path = "/:uuid", perm = "get")
+    public void getMessage(IServletData data, UUID uuid) {
+        Optional<IMessage> optMsg = messageService.getMessage(uuid);
+        if (!optMsg.isPresent()) {
+            data.sendError(HttpServletResponse.SC_NOT_FOUND, "Message with uuid " + uuid + " not found");
+            return;
+        }
+
+        data.addData("ok", true, false);
+        data.addData("message", optMsg.get(), true);
+    }
 
     @Endpoint(method = HttpMethod.POST, path = "/", perm = "create")
     public void sendMessage(ServletData data) {
@@ -29,6 +53,14 @@ public class MessageServlet extends BaseServlet {
             return;
         }
 
-        data.addData("ok", messageService.sendMessage(msg), false);
+        boolean allSent = messageService.sendMessage(msg);
+
+        if (allSent) {
+            data.setStatus(HttpServletResponse.SC_CREATED);
+            data.setHeader("Location", msg.getLink());
+        }
+
+        data.addData("ok", allSent, false);
+        data.addData("message", msg, true);
     }
 }

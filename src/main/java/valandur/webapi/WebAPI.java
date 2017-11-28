f4cf9fc511055e9b9d0b53c586bf27aa6735aaa2
Valandur <inithilian@gmail.com>
2017-11-28 12:34:01 +0100

    fix(web-api): Update outdated plugin description

--- src/main/java/valandur/webapi/WebAPI.java
+++ src/main/java/valandur/webapi/WebAPI.java
@@ -129,7 +129,7 @@ public class WebAPI {
     public static final String ID = "webapi";
     public static final String NAME = "Web-API";
     public static final String VERSION = "@version@";
-    public static final String DESCRIPTION = "Access Minecraft through a Web API";
+    public static final String DESCRIPTION = "Access Minecraft through a RESTful web server and an admin panel";
     public static final String URL = "https://github.com/Valandur/Web-API";
     private static final String UPDATE_URL = "https://api.github.com/repos/Valandur/Web-API/releases/latest";
 

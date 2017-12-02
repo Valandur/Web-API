174f046ee672feef3210662e006aba0629f4c3ac
Valandur <inithilian@gmail.com>
2017-12-02 16:38:18 +0100

    fix(view): Remove wrapping element from flying data

--- src/main/java/valandur/webapi/serialize/view/data/FlyingDataView.java
+++ src/main/java/valandur/webapi/serialize/view/data/FlyingDataView.java
@@ -1,10 +1,12 @@
 package valandur.webapi.serialize.view.data;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import org.spongepowered.api.data.manipulator.mutable.entity.FlyingData;
 import valandur.webapi.api.serialize.BaseView;
 
 public class FlyingDataView extends BaseView<FlyingData> {
 
+    @JsonValue
     public boolean flying;
 
 

749c5c0eb64b30883d3b72c68cb832ed818a4259
Valandur <inithilian@gmail.com>
2017-12-09 13:16:58 +0100

    feat(commands): Add clickable actions to user list command

--- src/main/java/valandur/webapi/command/user/CmdUserList.java
+++ src/main/java/valandur/webapi/command/user/CmdUserList.java
@@ -7,6 +7,8 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.text.format.TextColors;
 import valandur.webapi.user.UserPermission;
 import valandur.webapi.user.Users;
 
@@ -20,7 +22,19 @@ public class CmdUserList implements CommandExecutor {
         Collection<UserPermission> users = Users.getUsers();
         PaginationList.builder()
                 .title(Text.of("Web-API users"))
-                .contents(users.stream().map(u -> Text.of(u.getUsername())).collect(Collectors.toList()))
+                .contents(users.stream().map(u -> {
+                    Text editUser = Text.builder("[Change pw]")
+                            .color(TextColors.YELLOW)
+                            .onClick(TextActions.suggestCommand("/webapi users changepw " + u.getUsername() + " "))
+                            .build();
+
+                    Text rmvUser = Text.builder("[Remove]")
+                            .color(TextColors.RED)
+                            .onClick(TextActions.suggestCommand("/webapi users remove " + u.getUsername()))
+                            .build();
+
+                    return Text.builder(u.getUsername() + " ").append(editUser, rmvUser).build();
+                }).collect(Collectors.toList()))
                 .sendTo(src);
         return CommandResult.success();
     }

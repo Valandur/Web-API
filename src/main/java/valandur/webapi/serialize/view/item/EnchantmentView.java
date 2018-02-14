c2897b377b166f4763f600e0034914b61f31002f
Valandur <inithilian@gmail.com>
2018-02-14 20:07:32 +0100

    docs(enchantment): Add docs for EnchantmentView

--- src/main/java/valandur/webapi/serialize/view/item/EnchantmentView.java
+++ src/main/java/valandur/webapi/serialize/view/item/EnchantmentView.java
@@ -1,19 +1,31 @@
 package valandur.webapi.serialize.view.item;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.spongepowered.api.item.enchantment.Enchantment;
 import org.spongepowered.api.item.enchantment.EnchantmentType;
 import valandur.webapi.api.serialize.BaseView;
 
+@ApiModel("Enchantment")
 public class EnchantmentView extends BaseView<Enchantment> {
 
-    public EnchantmentType enchantment;
-    public int level;
+    @ApiModelProperty("The unique id of the enchantment type")
+    public String getId() {
+        return value.getType().getId();
+    }
+
+    @ApiModelProperty("The name of the enchantment type")
+    public String getName() {
+        return value.getType().getTranslation().get();
+    }
+
+    @ApiModelProperty("The level of the enchantment")
+    public int getLevel() {
+        return value.getLevel();
+    }
 
 
     public EnchantmentView(Enchantment value) {
         super(value);
-
-        this.enchantment = value.getType();
-        this.level = value.getLevel();
     }
 }
